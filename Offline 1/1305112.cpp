69c11a42c1deb81cd7744db6be5ab96e1bde4812
rifatarefin <rifatarefin19@gmail.com>
2018-04-18 20:11:34 +0600

    3

--- Offline 1/1305112.cpp
+++ Offline 1/1305112.cpp
@@ -15,6 +15,7 @@ bool compareByAngle(const point &a, const point &b)
 }
 point bottom;
 vector<point>points;
+vector<point>hull;
 
 void input()
 {
@@ -49,6 +50,20 @@ double dist(point a)
     return (a.x-bottom.x)*(a.x-bottom.x)+(a.y-bottom.y)*(a.y-bottom.y);
 }
 
+int checkTurn(point z)
+{
+    int len=hull.size();
+    point b=hull[len-1];
+    point a=hull[len-2];
+    int v1x=b.x-a.x;
+    int v1y=b.y-a.y;
+    int v2x=z.x-a.x;
+    int v2y=z.y-a.y;
+    return v1x*v2y-v1y*v2x;
+
+
+}
+
 int main()
 {
 
@@ -66,7 +81,7 @@ int main()
         if(points[i].x<lf.x)lf=points[i];
         if(points[i].x>rt.x)rt=points[i];
         if(points[i].y>top.y)top=points[i];
-        if(points[i].y<=bottom.x)
+        if(points[i].y<=bottom.y)
         {
             if(points[i].x>bottom.x)bottom=points[i];
         }
@@ -94,10 +109,22 @@ int main()
         }
         else it++;
     }
+    hull.push_back(bottom);
+    hull.push_back(points[0]);
+    for(vector<point>::iterator it=points.begin()+1;it!=points.end();it++)
+    {
+        int turn=checkTurn(*it);
+        if(turn>0)hull.push_back(*it);
+        while(turn<0)
+        {
+            hull.pop_back();
+            turn=checkTurn(*it);
+        }
+    }
 
-    for(vector<point>::iterator it=points.begin();it!=points.end();it++)
+    for(vector<point>::iterator it=hull.begin();it!=hull.end();it++)
     {
         point tr=*it;
-        cout<<tr.x<<" "<<tr.y<<" "<<tr.angle<<endl;
+        cout<<tr.x<<" "<<tr.y<<" "<<endl;//tr.angle<<endl;
     }
 }
