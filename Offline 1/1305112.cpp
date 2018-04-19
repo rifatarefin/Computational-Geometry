f5d8291bf9457f8a96406efdac8542467816c1f3
rifatarefin <rifatarefin19@gmail.com>
2018-04-19 19:10:30 +0600

    6

--- Offline 1/1305112.cpp
+++ Offline 1/1305112.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<GL/glut.h>
 using namespace std;
 #define pi (2*acos(0.0))
 ifstream fin("in.txt");
@@ -103,8 +104,151 @@ bool checkBoundery(point z, vector<point>v)
     return true;
 }
 
-int main()
+
+double cameraHeight;
+double cameraAngle;
+int drawgrid;
+int drawaxes;
+double angle_;
+
+void drawAxes()
 {
+    if(drawaxes==1)
+    {
+        glColor3f(1.0, 1.0, 1.0);
+        glBegin(GL_LINES);
+        {
+            glVertex3f( 100,0,0);
+            glVertex3f(-100,0,0);
+
+            glVertex3f(0,-100,0);
+            glVertex3f(0, 100,0);
+
+            glVertex3f(0,0, 100);
+            glVertex3f(0,0,-100);
+        }
+        glEnd();
+    }
+}
+
+
+
+void display()
+{
+
+    //clear the display
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0,0,0,0);	//color black
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /********************
+    / set-up camera here
+    ********************/
+    //load the correct matrix -- MODEL-VIEW matrix
+    glMatrixMode(GL_MODELVIEW);
+
+    //initialize the matrix
+    glLoadIdentity();
+
+    //now give three info
+    //1. where is the camera (viewer)?
+    //2. where is the camera looking?
+    //3. Which direction is the camera's UP direction?
+
+    //gluLookAt(100,100,100,	0,0,0,	0,0,1);
+    //gluLookAt(200*cos(cameraAngle), 200*sin(cameraAngle), cameraHeight,		0,0,0,		0,0,1);
+    gluLookAt(0,0,200,	0,0,0,	0,1,0);
+
+
+    //again select MODEL-VIEW
+    glMatrixMode(GL_MODELVIEW);
+
+
+    /****************************
+    / Add your objects from here
+    ****************************/
+    //add objects
+
+
+    glScalef(10,10,1);
+    for(vector<point>::iterator it=hull.begin()+1; it!=hull.end(); it++)
+    {
+        point tr=*it;
+        //cout<<tr.x<<" "<<tr.y<<" "<<endl;//tr.angle<<endl;
+        point nx=*(it-1);
+
+        glColor3f(1.0, 0.1, 1.0);
+        glBegin(GL_LINES);
+        {
+            glVertex3f( nx.x,nx.y,0);
+            glVertex3f(tr.x,tr.y,0);
+
+        }
+        glEnd();
+    }
+
+    point tr=*(hull.begin());
+    //cout<<tr.x<<" "<<tr.y<<" "<<endl;//tr.angle<<endl;
+    point nx=hull[hull.size()-1];
+
+    glColor3f(1.0, 0.1, 1.0);
+    glBegin(GL_LINES);
+    {
+        glVertex3f( nx.x,nx.y,0);
+        glVertex3f(tr.x,tr.y,0);
+
+    }
+    glEnd();
+
+    drawAxes();
+
+
+
+    //ADD this line in the end --- if you use double buffer (i.e. GL_DOUBLE)
+    glutSwapBuffers();
+}
+
+
+void animate()
+{
+    //angle+=0.05;
+    //codes for any changes in Models, Camera
+    glutPostRedisplay();
+}
+
+void init()
+{
+    //codes for initialization
+    drawgrid=0;
+    drawaxes=1;
+    cameraHeight=150.0;
+    cameraAngle=1.0;
+    angle_=0;
+
+    //clear the screen
+    glClearColor(0,0,0,0);
+
+    /************************
+    / set-up projection here
+    ************************/
+    //load the PROJECTION matrix
+    glMatrixMode(GL_PROJECTION);
+
+    //initialize the matrix
+    glLoadIdentity();
+
+    //give PERSPECTIVE parameters
+    gluPerspective(80,	1,	1,	1000.0);
+    //field of view in the Y (vertically)
+    //aspect ratio that determines the field of view in the X direction (horizontally)
+    //near distance
+    //far distance
+}
+
+int main(int argc, char **argv)
+{
+
+
 
     input();
     int len;
@@ -219,7 +363,27 @@ int main()
     {
         point tr=*it;
         cout<<tr.x<<" "<<tr.y<<" "<<endl;//tr.angle<<endl;
+
     }
+
+    glutInit(&argc,argv);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);	//Depth, Double buffer, RGB color
+
+    glutCreateWindow("My OpenGL Program");
+
+    init();
+
+    glEnable(GL_DEPTH_TEST);	//enable Depth Testing
+
+    glutDisplayFunc(display);	//display callback function
+    glutIdleFunc(animate);		//what you want to do in the idle time (when no drawing is occuring)
+
+    glutMainLoop();
+
+
+
 }
 
 
